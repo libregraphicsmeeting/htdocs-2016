3878216049653d9b2ac0e015192a341f32ec6a06
Manuel Schmalstieg <git.code@ms-studio.net>
2015-11-29 17:57:57 +0100

    Adding a "Room" taxonomy for the talk post type.

--- plugins/lgm-talks-2016/lgm-custom-post-types.php
+++ plugins/lgm-talks-2016/lgm-custom-post-types.php
@@ -75,6 +75,38 @@ function lgm_register_post_types() {
 			 		'singular_label' => 'Talk Status'
 			 		) 
 		);	
+
+// Add a "Room" taxonomy
+
+		register_taxonomy( 
+					'room',
+					array( 'talk' ), // = $object_type
+					array( 
+			 		'hierarchical' => true, 
+			 		'label' => 'Room',
+			 		'labels'  => array(
+			 			'name'                => 'Rooms',
+			 			'singular_name'       => 'Room',
+			 			'search_items'        => __( 'Search' ),
+			 			'popular_items'              => __( 'Most used' ),
+			 					'all_items'                  => __( 'All' ),
+			 					'parent_item'                => null,
+			 					'parent_item_colon'          => null,
+			 					'edit_item'                  => __( 'Edit Room' ),
+			 					'update_item'                => __( 'Update Room' ),
+			 					'add_new_item'               => __( 'New Room' ),
+			 					'new_item_name'              => __( 'New Room' ),
+			 					'separate_items_with_commas' => __( 'Separage with commas' ),
+			 					'add_or_remove_items'        => __( 'Add or remove' ),
+			 					'choose_from_most_used'      => __( 'Choose from most used' ),
+			 					'not_found'                  => __( 'No Room found' ),
+			 			'menu_name'           => __( 'Rooms' )
+			 		),
+			 		'public' => true,
+			 		'show_admin_column' => true,
+			 		'singular_label' => 'Room'
+			 		) 
+		);	
 	
 }
 
