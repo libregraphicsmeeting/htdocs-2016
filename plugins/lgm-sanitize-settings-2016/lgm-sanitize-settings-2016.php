6d2451796ca45baeba195426a2a3d5147bdf0ad6
Manuel Schmalstieg <git.code@ms-studio.net>
2015-12-07 15:19:01 +0100

    disable automatic core updates for now.

--- plugins/lgm-sanitize-settings-2016/lgm-sanitize-settings-2016.php
+++ plugins/lgm-sanitize-settings-2016/lgm-sanitize-settings-2016.php
@@ -16,8 +16,10 @@ Author URI: https://github.com/libregraphicsmeeting/infrastructure/
 
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
-// add_filter( 'allow_major_auto_core_updates', '__return_true' );
 
+add_filter( 'allow_dev_auto_core_updates', '__return_false' );    
+add_filter( 'allow_minor_auto_core_updates', '__return_false' ); 
+add_filter( 'allow_major_auto_core_updates', '__return_false' );
 
 
 /* Allowed FileTypes
